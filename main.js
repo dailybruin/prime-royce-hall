53b30c20228263b17027d96d32d7d6eb0963fa34
eliseyuen <skate4fun88@gmail.com>
2016-02-14 21:33:24 -0800

    add scrollify js

---
+++ main.js
@@ -0,0 +1,13 @@
+$.scrollify({
+    section : ".section-class-name",
+    sectionName : "section-name",
+    easing: "easeOutExpo",
+    scrollSpeed: 1100,
+    offset : 0,
+    scrollbars: true,
+    standardScrollElements: "",
+    before:function() {},
+    after:function() {},
+    afterResize:function() {},
+    afterRender:function() {}
+});
